8e813f5b9822767ea68a089ad67616bbbca264f2
priyadas06 <priyasingatala@gmail.com>
2025-11-13 20:56:04 +0100

    remove un-necessary code

--- automated_library_storage_system/src/application/modules/Human.java
+++
@@ -1,6 +0,0 @@
-package application.modules;
-
-// i made the human class for executing administration tasks
-public class Human {
-    
-}
--- automated_library_storage_system/src/application/modules/StorageManger.java
+++
@@ -1,9 +0,0 @@
-package application.modules;
-
-public class StorageManger {
-    // shelves
-
-
-    // get book by id
-    // add book (to an available shelf of corresponding category)
-}
